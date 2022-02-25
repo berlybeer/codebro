19b55dc5cc9c706f0648e3526986fcb39986660f
berlybeer <b.pumacajia.cz@gmail.com>
2022-02-25 13:12:59 -0500

    002 Review Java variables, data types and initialization

---
+++ 002 Java Variables/src/JavaVariables/Main.java
@@ -0,0 +1,55 @@
+package JavaVariables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x; //declaration
+		
+		x = 123; //assignment
+		
+		
+		int y = 124; //initialization
+		
+		System.out.println("My number x is: " + x + "\nand my number y is: " + y);
+		
+		
+		long z = 465792131321321L;
+		
+		System.out.println("My long number z is: " + z);
+		
+		byte a = 127;
+		
+		
+		
+		System.out.println("My byte number a is: " + a);
+		
+		float b = 3.14f;
+		
+		System.out.println("My float number is: " + b);
+		
+		double c = 3.14;
+		
+		System.out.println("My double number is: " + c);
+		
+		boolean d = true;
+		
+		
+		System.out.println("My bolean have the value: " + d);
+		
+		char symbol = '@';
+		
+		
+		System.out.println("My char have the value: " + symbol);
+		
+		String name = "Berly";
+		
+		System.out.println("Hello " + name);
+		
+		
+		
+
+	}
+
+}
