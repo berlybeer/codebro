abb31a9a2e2c70fec63ac95325d08eb5c2b64090
berlybeer <b.pumacajia.cz@gmail.com>
2022-02-25 13:33:24 -0500

    003 Java swap two variables

---
+++ 003 Java Swap two variables/src/SwapVariables/Main.java
@@ -0,0 +1,25 @@
+package SwapVariables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String x = "water";
+		String y = "Kool-Aid";
+		
+		String temp;
+		
+		temp = x;
+		x=y;
+		
+		y=temp;
+		
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+		
+
+	}
+
+}
