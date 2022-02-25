637d3e53b8e1fabca75d0afd06871f8b6ac39142
berlybeer <b.pumacajia.cz@gmail.com>
2022-02-25 12:43:04 -0500

    001 Create a first java project and java class

---
+++ 001 FirstProgram/src/FIrstProgram/Main.java
@@ -0,0 +1,12 @@
+package FIrstProgram;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I love pizza!");
+		System.out.println("It's really good");
+
+	}
+
+}
